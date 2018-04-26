83f3db6c48e2c664539c960acd743a82fb73ccba
arthurcordova <arthur.stapassoli@gmail.com>
2018-04-26 11:24:58 -0300

    Notification builder.

--- app/src/main/java/br/com/mobway/agendai/push/FirebaseMessagingService.kt
+++ app/src/main/java/br/com/mobway/agendai/push/FirebaseMessagingService.kt
@@ -1,24 +1,15 @@
 package br.com.mobway.agendai.push
 
-import android.app.NotificationManager
-import android.content.Context
-import android.support.v4.app.NotificationCompat
-import br.com.mobway.agendai.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class FirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
-        val notificationBuilder = NotificationCompat.Builder(this)
-        notificationBuilder.setContentTitle("Pele Medical Center")
-        notificationBuilder.setContentText(remoteMessage!!.notification!!.body)
-        notificationBuilder.setSmallIcon(R.drawable.ic_android)
-        notificationBuilder.setAutoCancel(true)
-        notificationBuilder.setVibrate(longArrayOf(500, 500, 500))
 
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, notificationBuilder.build())
+        MessageNotification
+                .notify(applicationContext, remoteMessage!!.notification!!.body!!, 1)
+
     }
 
     companion object {
